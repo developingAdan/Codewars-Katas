2a433853743f8201a42a81ce7014bf55f6120666
AdanGarciaCPU <developingadan@gmail.com>
2022-11-08 14:55:46 -0600

    Coding Meetup #1 -- Count # of JS devs that live in Europe

---
+++ 7_kyu/solution036.js
@@ -0,0 +1,53 @@
+// Title -- Coding Meetup #1 -- Count the number of JS devs from Europe?
+// Category -- Arrays
+// Tags -- Data Structures, Fundamentals, Algorithms, Strings, Arrays, Functional Programming
+
+// You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+// Your task is to return the number of JavaScript developers coming from Europe.
+
+// For example, given the following list:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//   { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//   { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//   { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+// ];
+
+// your function should return number 1.
+
+// If, there are no JavaScript developers from Europe then your function should return 0.
+
+// Notes:
+
+//     The format of the strings will always be Europe and JavaScript.
+//     All data will always be valid and uniform as in the example above.
+
+// My Solution
+function countDevelopers(list) {
+    // your awesome code here :)
+    
+    let filtered = list.filter(item => item.continent === 'Europe' && item.language === 'JavaScript')
+    
+    if (filtered.length === 0) {
+        return 0
+      }
+    
+    return filtered.length
+  }
+
+
+// Other Solutions
+
+function countDevelopers(list) {
+    return list.filter(x=>x.continent=='Europe'&&x.language=='JavaScript').length
+  }
+
+
+  function countDevelopers(list) {
+    return list.reduce((count, dev) => 
+      (dev.continent === 'Europe' && dev.language === 'JavaScript')
+        ? count + 1 : count
+      , 0);
+  }
